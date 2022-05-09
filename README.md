9e05eb59d5a84bf70faab1d6dd0a77456bb8a572
yoseph <diddytpq5@gmail.com>
2022-05-09 09:34:59 +0900

    ADD: rplidar ros package

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Slam_using_rplidar
+
+## lidar port check
+### ls /dev/ttyUSB* 
+
+## give port permission
+### sudo chmod 666 /dev/ttyUSB0
+
+## view lidar sensor value using rviz
+### roslaunch rplidar_ros view_rplidar.launch
---
+++ build/rplidar_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yoseph/worckspace/Slam_using_rplidar/src/rplidar_ros
+# Build directory: /home/yoseph/worckspace/Slam_using_rplidar/build/rplidar_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
